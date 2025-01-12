38f91aabe71a8e3c33b4fc7b5793c009de718bea
Кирилл Фунтов <funtaps@gmail.com>
2025-01-12 21:16:02 +0400

    Lazy load images

--- src/components/ImageStar.tsx
+++ src/components/ImageStar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { Vector3 } from "three";
 
 import { Center, Image } from "@react-three/drei";
@@ -20,18 +20,18 @@ export const ImageStar = ({
   const divider = (width > height ? width : height) / 10;
   return (
     <>
-      <Image url={finalUrl} scale={[0, 0]} position={[0, 0, 0]} />
       <BaseStar
         position={position}
         onToggle={() => setIsPlaying((state) => !state)}
         isOn={isPlaying}
         showChildOnHover={false}
         showChildOnOn={true}
-        // childMargin={6.5}
         accent={"#ff0000"}
       >
         <Center position={[0, height / divider / 2 + 1.5, 0]}>
-          <Image url={finalUrl} scale={[width / divider, height / divider]} />
+          <Suspense fallback={null}>
+            <Image url={finalUrl} scale={[width / divider, height / divider]} />
+          </Suspense>
         </Center>
       </BaseStar>
     </>
